8e56173be2a3be280b8bb925fc5888c20d5dfd7f
Cheick Diarra <diarra.tiemoko@mca.christuniversity.in>
2016-01-02 23:02:17 +0530

    Random Api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var random_people = require('../src/random/api');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/random', function(req , res) {
+
+  random_people.generate(req.query)
+  .then(function(result) {
+      console.log('Generated');
+      res.status(200).json(result);
+  });
 });
 
 module.exports = router;
